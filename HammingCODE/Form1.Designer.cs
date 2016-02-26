f7feec20649b48001919ebdd5134cb732f2cd019
Muhammad Furqan <mfurqanam19@gmail.com>
2016-02-26 16:31:48 +0500

    Hamming code program

---
+++ HammingCODE/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(124, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(413, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(124, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(413, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send bits";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(121, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(28, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Recieved bits :";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(12, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Check error in bit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(124, 102);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(413, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(157, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(286, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hamming  Code Program";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(549, 222);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Software by Muhammad Furqan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ HammingCODE/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        static int paritybits = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string mys = textBox1.Text;
+            string[] my = new string[mys.Length];
+            char[] m = new char[mys.Length];
+            int i = 0, k = 0, c = 1, b = 0, d = 4, flag = 0;
+            double r = 0, y = 0, j = 0;    
+                       
+            for (i = 0; i < mys.Length; i++)
+                {
+
+                    my[i] = (mys[i].ToString());
+                    if (my[i] != "1" && my[i] != "0" )
+                     
+                        {
+                            textBox3.Text = "You can enter only binary bits";
+                            flag = 1;
+                            break;
+                        }
+                }
+
+            if (flag == 0)
+
+            {
+                label2.Text = "";
+                textBox2.Clear();
+                if (mys.Length == 2)
+                    paritybits = 3;
+                else if (mys.Length == 1)
+                    paritybits = 2;
+                else
+                {
+                    for (i = 0; i <= mys.Length; i++)
+                    {
+                        r = Math.Pow(2, i);
+                        y = i + mys.Length + 1;
+                        if (r >= y)
+                        {
+                            paritybits = i;
+                            break;
+                        }
+                    }
+                }
+
+                int[] data = new int[mys.Length + paritybits + 10];
+                for (i = 3, k = 0; i <= mys.Length + paritybits; i++, k++)
+                {
+                    data[i] = Convert.ToInt16(my[k]);
+
+                    if (i == d - 1)
+                    {
+                        i++;
+                        d *= 2;
+                    }
+
+                }
+
+
+                for (i = c; b < paritybits; i = c)
+                {
+
+                    for (k = c; k <= mys.Length + paritybits; k++)
+                    {
+                        data[i] = data[i] ^ data[k];
+                        if (k % c == c - 1)
+                        {
+                            k += c;
+                        }
+
+                    }
+
+                    b++;
+
+                    c = (c * 2);
+                }
+
+
+                for (i = 1; i <= mys.Length + paritybits; i++)
+                {
+
+
+                    textBox2.Text = textBox2.Text + data[i].ToString();
+
+                }
+
+               textBox3.Text = textBox2.Text;
+                if (mys == "")
+                {
+                    textBox3.Text = "Please enter some binary bits";
+                } 
+
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string mys1 = textBox2.Text;
+            string[] my1 = new string[mys1.Length];
+            int i = 0, k = 0, a = 0, c = 1, b = 0, d = 4;
+            int[] C_Values = new int[paritybits + 10];
+            bool flag=false;
+
+            for (i = 0; i < mys1.Length; i++)
+            {
+                my1[i] = mys1[i].ToString();
+                if (my1[i] != "1" && my1[i] != "0" )
+                {
+                    label2.Text = "You can enter only binary bits";
+                    flag = true;
+                    break;
+                }
+
+            }
+         if(   flag == false)
+            {
+                int[] data = new int[mys1.Length + 10];
+
+                for (i = 1, k = 0; k < mys1.Length; i++, k++)
+                {
+                    data[i] = Convert.ToInt16(my1[k]);
+
+                }
+
+
+                for (i = c; b < paritybits; i = c)
+                {
+
+                    for (k = c; k <= mys1.Length; k++)
+                    {
+                        C_Values[b] = C_Values[b] ^ data[k];
+                        if (k % c == c - 1)
+                        {
+                            k += c;
+                        }
+
+                    }
+
+                    if (C_Values[b] == 1)
+                    {
+                        a += i;
+                    }
+                    b++;
+                    c = (c * 2);
+
+                }
+                if (a == 0)
+                {
+                    label2.Text = "All bits are correct";
+                }
+                else
+                {
+                    label2.Text = "Bit no. "+a.ToString() + " is incorrect";
+                }
+//if (mys1 == "")
+                {
+                   // label2.Text = "Please enter some binary bits";
+                }
+                if (textBox2.Text.Length != textBox3.Text.Length || mys1 == "")
+                {
+                    label2.Text = "Put recieved bits in the textbox";
+                }
+                
+            }
+        }
+    }
+}
